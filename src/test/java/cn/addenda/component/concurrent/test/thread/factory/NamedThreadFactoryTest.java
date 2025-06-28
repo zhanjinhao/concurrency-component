cc843b18d5e5fe2f7b477fd2fea5e344a3ed5e3d
addenda <isjinhao@163.com>
2025-06-28 17:36:47 +0800

    feature: rejection policy & thread factory

---
+++ src/test/java/cn/addenda/component/concurrent/test/thread/factory/NamedThreadFactoryTest.java
@@ -0,0 +1,35 @@
+package cn.addenda.component.concurrent.test.thread.factory;
+
+import cn.addenda.component.base.util.SleepUtils;
+import cn.addenda.component.concurrent.thread.factory.NamedThreadFactory;
+import cn.addenda.component.concurrent.thread.factory.UncaughtThreadFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class NamedThreadFactoryTest {
+
+  @Test
+  public void test1() throws Exception {
+    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+            2,
+            4,
+            30,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(10),
+            UncaughtThreadFactory.of(NamedThreadFactory.of("NamedThreadFactoryTest", Executors.defaultThreadFactory())));
+    for (int i = 0; i < 14; i++) {
+      threadPoolExecutor.submit(() -> {
+        SleepUtils.sleep(TimeUnit.SECONDS, 1);
+        Assert.assertEquals("NamedThreadFactoryTest", Thread.currentThread().getName().split("-")[0]);
+      });
+    }
+
+    threadPoolExecutor.awaitTermination(5, TimeUnit.SECONDS);
+  }
+
+}
