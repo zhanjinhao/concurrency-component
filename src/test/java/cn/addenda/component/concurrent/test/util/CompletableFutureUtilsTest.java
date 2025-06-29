c5d3c31ab57bfa645c9194154bdc4dd130f4c39e
addenda <isjinhao@163.com>
2025-06-29 19:28:31 +0800

    feature: CompletableFutureUtilsTest

--- src/test/java/cn/addenda/component/concurrent/test/util/CompletableFutureUtilsTest.java
+++ src/test/java/cn/addenda/component/concurrent/test/util/CompletableFutureUtilsTest.java
@@ -24,6 +24,12 @@ public class CompletableFutureUtilsTest {
   @Test
   public void test1() {
 
+    AtomicReference<String> a1 = new AtomicReference<>(null);
+    AtomicReference<String> a2 = new AtomicReference<>(null);
+    AtomicReference<String> a3 = new AtomicReference<>(null);
+    AtomicReference<String> a4 = new AtomicReference<>(null);
+    AtomicReference<String> a5 = new AtomicReference<>(null);
+
     ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
             2,
             2,
@@ -34,68 +40,82 @@ public class CompletableFutureUtilsTest {
 
     log.info("start ");
     CompletableFuture<Void> f1 = CompletableFuture.runAsync(NamedRunnable.of("f1", name -> {
-      SleepUtils.sleep(TimeUnit.SECONDS, 30);
+      SleepUtils.sleep(TimeUnit.SECONDS, 3);
+      a1.set("a1");
       log.info(name + "执行完毕！");
     }), threadPoolExecutor);
-    CompletableFutureUtils.orTimeout(f1, 10, TimeUnit.SECONDS).handle(
+    CompletableFutureUtils.orTimeout(f1, 1, TimeUnit.SECONDS).handle(
             (unused, throwable) -> {
               log.error("f1" + throwable.toString());
               return null;
             });
 
     CompletableFuture<Void> f2 = CompletableFuture.runAsync(NamedRunnable.of("f2", name -> {
-      SleepUtils.sleep(TimeUnit.SECONDS, 30);
+      SleepUtils.sleep(TimeUnit.SECONDS, 3);
+      a2.set("a2");
       log.info(name + "执行完毕！");
     }), threadPoolExecutor);
-    CompletableFutureUtils.orTimeout(f2, 10, TimeUnit.SECONDS).handle(
+    CompletableFutureUtils.orTimeout(f2, 1, TimeUnit.SECONDS).handle(
             (unused, throwable) -> {
               log.error("f2" + throwable.toString());
               return null;
             });
 
     CompletableFuture<Void> f3 = CompletableFuture.runAsync(NamedRunnable.of("f3", name -> {
-      SleepUtils.sleep(TimeUnit.SECONDS, 30);
+      SleepUtils.sleep(TimeUnit.SECONDS, 3);
+      a3.set("a3");
       log.info(name + "执行完毕！");
     }), threadPoolExecutor);
-    CompletableFutureUtils.orTimeout(f3, 10, TimeUnit.SECONDS).handle(
+    CompletableFutureUtils.orTimeout(f3, 1, TimeUnit.SECONDS).handle(
             (unused, throwable) -> {
               log.error("f3" + throwable.toString());
               return null;
             });
 
     CompletableFuture<Void> f4 = CompletableFuture.runAsync(NamedRunnable.of("f4", name -> {
-      SleepUtils.sleep(TimeUnit.SECONDS, 30);
+      SleepUtils.sleep(TimeUnit.SECONDS, 3);
+      a4.set("a4");
       log.info(name + "执行完毕！");
     }), threadPoolExecutor);
-    CompletableFutureUtils.orTimeout(f4, 10, TimeUnit.SECONDS).handle(
+    CompletableFutureUtils.orTimeout(f4, 1, TimeUnit.SECONDS).handle(
             (unused, throwable) -> {
               log.error("f4" + throwable.toString());
               return null;
             });
 
     CompletableFuture<Void> f5 = CompletableFuture.runAsync(NamedRunnable.of("f5", name -> {
-      SleepUtils.sleep(TimeUnit.SECONDS, 30);
+      SleepUtils.sleep(TimeUnit.SECONDS, 3);
+      a5.set("a5");
       log.info(name + "执行完毕！");
     }), threadPoolExecutor);
-    CompletableFutureUtils.orTimeout(f5, 10, TimeUnit.SECONDS).handle(
+    CompletableFutureUtils.orTimeout(f5, 1, TimeUnit.SECONDS).handle(
             (unused, throwable) -> {
               log.error("f5" + throwable.toString());
               return null;
             });
 
-    new Thread(new Runnable() {
+    Thread monitorThread = new Thread(new Runnable() {
       @Override
       public void run() {
         while (true) {
-          SleepUtils.sleep(TimeUnit.SECONDS, 5);
+          SleepUtils.sleep(TimeUnit.MILLISECONDS, 200);
           log.info(threadPoolExecutor.toString());
         }
       }
-    }).start();
+    });
+    monitorThread.setDaemon(true);
+    monitorThread.start();
 
-    // 结论：超时后，正在执行中的任务会执行完成，排队的任务不会被线程执行
-    SleepUtils.sleep(TimeUnit.SECONDS, 300);
+    // 结论：
+    // 1、超时后，正在执行中的任务会执行完成，排队的任务不会被线程执行
+    // 2、排队中的任务被取消的原理：任务里设置了标志位。这些任务依然会被从Queue里取出来，但是不会执行我们设置的逻辑。这些任务依然被认为是completed tasks。
+    SleepUtils.sleep(TimeUnit.SECONDS, 5);
 
+    Assert.assertEquals("a1", a1.get());
+    Assert.assertEquals("a2", a2.get());
+    Assert.assertNull(a3.get());
+    Assert.assertNull(a4.get());
+    Assert.assertNull(a5.get());
   }
 
   @Test
